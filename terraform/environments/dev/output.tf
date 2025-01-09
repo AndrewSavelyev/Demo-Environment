6af872a618d689a66de4f5e8c9795f30285268a7
Andrii Savelyev <andrii.savelyev@dataart.com>
2025-01-09 12:54:43 +0200

    Release v.2

--- terraform/environments/dev/output.tf
+++ terraform/environments/dev/output.tf
@@ -7,13 +7,9 @@ output "eks1_cluster_name" {
     value                   = module.eks1.cluster_demo_name
     description             = "EKS cluster data"
 }
-<<<<<<< HEAD
 /*
 output "eks2_cluster_name" {
     value                   = module.eks2.cluster_demo_name
     description             = "EKS cluster data"
 }
 */
-=======
-
->>>>>>> 498bc6a9c31409c41bd5e7f42989dd73f8bda7e4
